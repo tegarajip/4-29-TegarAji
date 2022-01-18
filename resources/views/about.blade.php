5a0ff89da6d165a237d36fcd5b1e57a77b614d14
tegarajip <3103120219@student.smktelkom-pwt.sch.id>
2022-01-18 08:48:37 +0700

    gas

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Tegar | about</title>
+</head>
+<body>
+    <h1> halaman about </h1>
+    <h3> Tegar Aji Pangestu </h3>
+    <p> 3103120219@student.smktelkom-pwt.sch.id </p>
+    <img src="image/cr7.jpg" alt="">
+
+
+</body>
+</html>
